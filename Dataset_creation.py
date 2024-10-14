907b077cd4931437abf86be9461f64b78ab0348f
7376pawank <109576643+7376pawank@users.noreply.github.com>
2024-10-14 12:40:22 +0530

    Codes uploaded

--- Dataset_creation.py
+++ Dataset_creation.py
@@ -7,6 +7,7 @@ data_extracted=[]
 avg_headnotes = 0
 avg_judgements=0
 
+
 # Loop through the file numbers
 for i in range(1, 10001):
     file_name = f"Judgements/{i}.docx"  # Assuming the file names follow the pattern 'file1.docx', 'file2.docx', and so on
@@ -23,20 +24,22 @@ for i in range(1, 10001):
         print(i)
         file_contents.append(functions_for_data_extraction.combine_hyphenated_words(content))
         json_extract, len_of_hednotes, len_of_judgement = functions_for_data_extraction.json_data_func(file_contents[i-1])
-        json_data = json.dumps(json_extract, indent=4)
-        data_extracted.append(json_data)
+        data_extracted.append(json_extract)
         avg_headnotes = avg_headnotes+len_of_hednotes
         avg_judgements = avg_judgements+len_of_judgement
 
     except FileNotFoundError:
         print(f"File '{file_name}' not found.")
+
 print("mean headnotes sentences", int(avg_headnotes/10000))
 print("mean judgement sentences", int(avg_judgements/10000))
 
+json_data = json.dumps(data_extracted, indent=4)
+
 #CREATION OF JSON DATASET
-with open("data.json", "w") as json_file:
+with open("dataset.json", "w") as json_file:
     json.dump(data_extracted, json_file, indent=4)
 
-with open("data1.json", "r") as json_file:
+with open("dataset.json", "r") as json_file:
     dictionaries = json.load(json_file)
 
